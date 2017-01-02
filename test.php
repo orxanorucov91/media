b9cf8baddb16b5d7915f08bed5494ec2c1bc1b47
zamir <zamirmmmm@gmail.com>
2017-01-02 19:34:34 +0400

    fixed  bug
    Please enter the commit message for your changes. Lines starting

--- test.php
+++ test.php
@@ -11,5 +11,4 @@ $token = file_get_contents('news.txt');
 $tokens = preg_split('/((^\p{P}+)|(\p{P}*\s+\p{P}*)|(\p{P}+$))/', $token, -1, PREG_SPLIT_NO_EMPTY);
 
 $bigrams = NGramFactory::create($tokens);
-
-var_dump($bigrams);
+var_dump(array_count_values($bigrams));
