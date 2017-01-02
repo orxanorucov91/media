d60eb532a6b997c096278aceda536801dea72ebf
zamir <zamirmmmm@gmail.com>
2017-01-02 19:27:22 +0400

    ngram xeta aradan qaldirildi

--- test.php
+++ test.php
@@ -4,7 +4,11 @@ include 'autoloader.php';
 
 use api\NGrams\NGramFactory;
 
-$tokens = ['one','two','three'];
+$data = array();
+
+
+$token = file_get_contents('news.txt');
+$tokens = preg_split('/((^\p{P}+)|(\p{P}*\s+\p{P}*)|(\p{P}+$))/', $token, -1, PREG_SPLIT_NO_EMPTY);
 
 $bigrams = NGramFactory::create($tokens);
 
